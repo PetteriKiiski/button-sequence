fb06b7b2717c52faeda772f90a3c6b0526c8c88b
Petteri Kiiski <petteri.daniel.kiiski@gmail.com>
2022-11-20 19:43:24 -0800

    Moving scene with action list. Going to add character impact

--- ButtonSequence.py
+++ ButtonSequence.py
@@ -1,5 +1,5 @@
 #Setup
-import pygame, sys
+import pygame, sys, time
 from pygame.locals import *
 pygame.init()
 canvas = pygame.display.set_mode((1360, 660))
@@ -8,12 +8,20 @@ main = pygame.image.load("Homescreen.png")
 playerimg = pygame.image.load("Player.png")
 stoneimg = pygame.image.load("Stone.png")
 enemyimg = pygame.image.load("Enemy.png")
+finishimg = pygame.image.load("Finish.png")
+upimg = pygame.image.load("Up.png")
+downimg = pygame.image.load("Down.png")
+attackimg = pygame.image.load("Sword.png")
 #Classes
 class Player:
     def __init__(self, vel=1, health=10):
         self.vel = int(vel)
         self.health=int(health)
         self.y = 460
+        self.dieing = False
+        self.dietimer = time.time()
+    def move(self):
+        pass
 
 class Pebble:
     def __init__(self, player, x):
@@ -21,6 +29,8 @@ class Pebble:
         self.x = int(x)
         self.y = 560
         self.img = stoneimg
+    def move(self):
+        self.x -= self.vel
 
 class Enemy:
     def __init__(self, player, vel, x): #May add Attack Speed and Attack Strength
@@ -28,15 +38,25 @@ class Enemy:
         self.x = int(x)
         self.y = 460
         self.img = enemyimg
+    def move(self):
+        self.x -= self.vel
+
+class FinishLine:
+    def __init__(self, player, x):
+        self.vel = player.vel
+        self.x = x
+    def move(self):
+        self.x -= self.vel
+
 #Variables
 down = False #If space key is right now held down
 scene = 0 #Scene ID
 level = 1
-distance = 0
-finaldistance = 0
 lvlsprites = []
 player = Player()
+finaldistance = FinishLine(player, 0)
 seq = ["j"] #Key: j=jump, d=duck, a=attack, POSSIBLY, g=grab, u=use
+seqdict = {"j":upimg, "d":downimg, "a":attackimg}
 #MainLoop
 while True:
     canvas.fill((255, 255, 255))
@@ -62,16 +82,21 @@ while True:
                 seq = sprite[1:]
             elif sprite[0] == "Distance":
                 #final distance
-                finaldistance = int(sprite[1])
+                finaldistance = FinishLine(player, int(sprite[1]))
             else:
                 #Adds a sprite to sprite list
                 lvlsprites.append(spritedict[sprite[0]](player, *sprite[1:]))
         scene = 2
     if scene == 2:
         canvas.blit(playerimg, (200, player.y))
+        canvas.blit(finishimg, (finaldistance.x, 0))
+        finaldistance.move()
+        for i in range(len(seq)):
+            canvas.blit(seqdict[seq[i]], (i * 100, 0))
         for sprite in lvlsprites:
-#            if sprite.x + sprite.img.get_width() < 0 or sprite.x - sprite.img.get_width() > 1360: #If image is inside of this box
-            canvas.blit(sprite.img, (sprite.x, sprite.y))
+            if sprite.x + sprite.img.get_width() > 0 or sprite.x < 1360: #If image is inside of this box
+                canvas.blit(sprite.img, (sprite.x, sprite.y))
+            sprite.move()
     #Event loop (Only three events, since their is one key)
     for event in pygame.event.get():
         if event.type == QUIT:
