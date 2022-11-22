5786f136406cd1371976f854ddfc1f7b7b373611
Petteri Kiiski <petteri.daniel.kiiski@gmail.com>
2022-11-22 12:24:13 -0800

    Made level screen, now can make more levels

--- ButtonSequence.py
+++ ButtonSequence.py
@@ -104,6 +104,9 @@ class FinishLine:
 down = False #If space key is right now held down
 scene = 0 #Scene ID
 level = 1
+world = 1
+maxlevelworld = [1, 1]
+absolutelevelworld = [1, 1]
 lvlsprites = []
 player = Player()
 finaldistance = FinishLine(player, 0)
@@ -112,7 +115,7 @@ seqindex = 0
 seqdict = {"j":upimg, "d":downimg, "a":attackimg}
 font = pygame.font.Font("freesansbold.ttf", 100)
 clock = pygame.time.Clock()
-timer = time.time()
+leveltimer = time.time()
 #MainLoop
 while True:
     timer = time.time()
@@ -122,38 +125,47 @@ while True:
     if scene == 0.5:
         canvas.blit(notes, (0, 0))
     if scene == 1:
-        level = 1
-        lvlsprites = []
-        player = Player()
-        finaldistance = FinishLine(player, 0)
-        seq = ["j"] #Key: j=jump, d=duck, a=attack, POSSIBLY, g=grab, u=use
-        seqindex = 0
-        seqdict = {"j":upimg, "d":downimg, "a":attackimg}
-        font = pygame.font.Font("freesansbold.ttf", 100)
-        try:
-            with open("level" + str(level) + ".lvl", "r") as info:
-                txt = info.read()
-        except Exception as err:
-            print ("ERROR: " + str(err))
-        lvlinfo = txt.split("\n")
-        for info in lvlinfo:
-            if info == "":
-                continue
-            spritedict = {"Enemy":Enemy, "Pebble":Pebble}
-            sprite = info.split(":")
-            if sprite[0] == "Player":
-                #Player Sprite
-                player = Player(*sprite[1:])
-            elif sprite[0] == "Sequence":
-                #Space Sequence
-                seq = sprite[1:]
-            elif sprite[0] == "Distance":
-                #final distance
-                finaldistance = FinishLine(player, int(sprite[1]))
-            else:
-                #Adds a sprite to sprite list
-                lvlsprites.append(spritedict[sprite[0]](player, *sprite[1:]))
-        scene = 2
+        levelimg = pygame.image.load("LevelTemplate" + str(level) + ".png")
+        canvas.blit(levelimg, (0, 0))
+        text = font.render("World " + str(world), True, (0, 0, 0), None)
+        canvas.blit(text, (0, 0))
+        text = font.render("Press space to next level", True, (0, 0, 0), None)
+        canvas.blit(text, (0, 250))
+        text = font.render("or wait to start", True, (0, 0, 0), None)
+        canvas.blit(text, (0,350))
+        if time.time() - leveltimer >= 3:
+            level = 1
+            lvlsprites = []
+            player = Player()
+            finaldistance = FinishLine(player, 0)
+            seq = ["j"] #Key: j=jump, d=duck, a=attack, POSSIBLY, g=grab, u=use
+            seqindex = 0
+            seqdict = {"j":upimg, "d":downimg, "a":attackimg}
+            font = pygame.font.Font("freesansbold.ttf", 100)
+            try:
+                with open("level" + str(level) + ".lvl", "r") as info:
+                    txt = info.read()
+            except Exception as err:
+                print ("ERROR: " + str(err))
+            lvlinfo = txt.split("\n")
+            for info in lvlinfo:
+                if info == "":
+                    continue
+                spritedict = {"Enemy":Enemy, "Pebble":Pebble}
+                sprite = info.split(":")
+                if sprite[0] == "Player":
+                    #Player Sprite
+                    player = Player(*sprite[1:])
+                elif sprite[0] == "Sequence":
+                    #Space Sequence
+                    seq = sprite[1:]
+                elif sprite[0] == "Distance":
+                    #final distance
+                    finaldistance = FinishLine(player, int(sprite[1]))
+                else:
+                    #Adds a sprite to sprite list
+                    lvlsprites.append(spritedict[sprite[0]](player, *sprite[1:]))
+            scene = 2
     if scene == 2:
         canvas.blit(playerimg, (200, player.y))
         player.move()
@@ -161,7 +173,7 @@ while True:
         finaldistance.move()
         for i in range(len(seq)):
             canvas.blit(seqdict[seq[i]], (i * 100, 0))
-        text = font.render("Health: " + str(player.health), True, (0, 0, 0), (255, 255, 255, 255))
+        text = font.render("Health: " + str(player.health), True, (0, 0, 0), None)
         canvas.blit(text, (0, 100))
         for sprite in lvlsprites:
             if (sprite.x + sprite.img.get_width() > 0 or sprite.x < 1360) and not sprite.dead: #If image is inside of this box
@@ -192,6 +204,12 @@ while True:
                 scene = 0.5
             elif scene == 0.5:
                 scene = 1
+                leveltimer = time.time()
+            elif scene == 1:
+                level += 1
+                if level > maxlevelworld[0]:
+                    level = 1
+                leveltimer = time.time()
             elif scene == 2:
                 if seq[seqindex] == "j":
                     player.jump()
@@ -204,8 +222,10 @@ while True:
                     seqindex = 0
             elif scene == 3:
                 scene = 1
+                leveltimer = time.time()
             elif scene == 4:
                 scene = 1
+                leveltimer = time.time()
         elif event.type == KEYUP and event.key == K_SPACE:
             down = False
     clock.tick(60)
