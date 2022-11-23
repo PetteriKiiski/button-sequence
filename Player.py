548052ecacac88acb2b8ceb5e54957ecb8819ff1
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-11-23 15:31:43 -0800

    squished player

--- Player.py
+++ Player.py
@@ -13,15 +13,17 @@ class Player:
         self.contact = False
         self.dietimer = time.time()
         self.jumptimer = time.time()
-        self.img = pygame.image.load("assets/images/Player.png");
+        self.img = pygame.image.load("assets/images/Player.png")
 
     def move(self):
+        self.img = pygame.image.load("assets/images/Player.png")
         if self.jumping:
             self.y = 260
             if time.time() - self.jumptimer >= self.timing:
                 self.jumping = False
         elif self.ducking:
             self.y = 560
+            self.img = pygame.image.load("assets/images/PlayerSquish.png")
             if time.time() - self.ducktimer >= self.timing:
                 self.ducking = False
         else:
